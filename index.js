1cf6a77a511e5b6e5dd793b0ca67e6a2adc910f1
Rakib-cloud <ri1641715@gmail.com>
2023-01-18 14:38:32 +0600

    banner responsive

--- index.js
+++ index.js
@@ -40,3 +40,8 @@ function changeSlide(){
 
 
 }
+const navLinks = document.querySelector('.nav-links')
+function onToggleMenu(e){
+    e.name = e.name === 'menu' ? 'close' : 'menu'
+    navLinks.classList.toggle('top-[9%]')
+}
